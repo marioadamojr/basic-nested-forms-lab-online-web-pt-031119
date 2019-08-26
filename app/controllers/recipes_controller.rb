f194c11b3d2c36bcd6f671dc44b231450939fed9
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-26 22:33:49 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(address_type: 'work')
+    @recipe.ingredients.build(address_type: 'home')
   end
 
   def create
