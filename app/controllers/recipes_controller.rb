0ab1bf0fa07ddac490f5345d61235d430ca63b90
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-26 22:36:11 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,5 +14,21 @@ class RecipesController < ApplicationController
   end
 
   def create
+    Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+    def person_params
+    params.require(:person).permit(
+      :name,
+      addresses_attributes: [
+        :street_address_1,
+        :street_address_2,
+        :city,
+        :state,
+        :zipcode,
+        :address_type
+      ]
+    )
   end
 end
